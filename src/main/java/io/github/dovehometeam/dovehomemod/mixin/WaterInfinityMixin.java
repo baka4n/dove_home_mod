ad7b3e5181cf3e7c3ebf9f6691e3fd0c2a76f2a3
baka4n <qq474899581@gmail.com>
2024-06-02 21:51:51 +0800

    更新水资源紧缺

---
+++ src/main/java/io/github/dovehometeam/dovehomemod/mixin/WaterInfinityMixin.java
@@ -0,0 +1,17 @@
+package io.github.dovehometeam.dovehomemod.mixin;
+
+import io.github.dovehometeam.dovehomemod.Config;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.material.WaterFluid;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(WaterFluid.class)
+public class WaterInfinityMixin {
+    @Inject(method = "canConvertToSource", at = @At("HEAD"), cancellable = true)
+    private void canConvertToSource(Level pLevel, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(!Config.noInfiniteWaterBoolean);
+    }
+}

46e208d701cf98e987f3510ee9759f2e6fd30f53
baka4n <qq474899581@gmail.com>
2024-07-19 21:32:47 +0800

    模块注册完全

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,9 +18,27 @@ plugins {
 
 rootProject.name = "dove-modules"
 
+include("lib")
 include("dovehome")
 include("db")
 include("god")
+include("permissions")
+include("datagen")
+include("miner")
+include("team")
+include("quest")
+include("map")
+include("query")
+include("command")
+project(":lib").name = "dovelib"
 project(":dovehome").name = "dovehomemod"
 project(":db").name = "dovedb"
 project(":god").name = "dovegod"
+project(":permissions").name = "dovepermissions"
+project(":datagen").name = "dovedatagen"
+project(":miner").name = "doveminer"
+project(":team").name = "doveteam"
+project(":quest").name = "dovequest"
+project(":map").name = "dovemap"
+project(":query").name = "dovequery"
+project(":command").name = "dovecommand"
